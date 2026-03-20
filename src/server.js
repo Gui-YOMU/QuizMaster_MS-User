0c8533fac12d99fee78d41815e47c661ab063e7b
= <=>
2026-03-20 15:15:23 +0100

    Modification nom port

--- src/server.js
+++ src/server.js
@@ -4,21 +4,21 @@ import cors from "cors";
 
 import { userRouter } from "./routers/userRouter.js";
 
-const PORT = process.env.PORT;
+const USER = process.env.USER;
 
-const app = express();
+const APP = express();
 
-app.use(cors());
-app.use(express.json());
+APP.use(cors());
+APP.use(express.json());
 
-app.get("/", (req, res) => {
+APP.get("/", (req, res) => {
   res.json({ page: "login" });
 });
 
-app.use(userRouter);
+APP.use(userRouter);
 
-app.listen(PORT, (error) => {
+APP.listen(USER, (error) => {
   error
     ? console.error(error)
-    : console.log(`Connecté avec succès au serveur localhost:${PORT}.`);
+    : console.log(`Connecté avec succès au serveur local ${USER}.`);
 });
